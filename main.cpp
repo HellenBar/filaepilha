fb99031d6589827f91f4453ca42039f8322b787f
HellenBar <166183292+HellenBar@users.noreply.github.com>
2024-11-04 15:07:02 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,50 +4,48 @@
 typedef struct {
     int itens[TAMANHO];
     int topo;
-} PilhaCircular;
+} Pilha;
 
 // Inicializa a pilha
-void inicializarPilha(PilhaCircular *pilha) {
+void inicializarPilha(Pilha *pilha) {
     pilha->topo = -1;
 }
 
-// Verifica se a pilha está cheia
-int estaCheia(PilhaCircular *pilha) {
-    return ((pilha->topo + 1) % TAMANHO == 0 && pilha->topo != -1);
-}
-
 // Verifica se a pilha está vazia
-int estaVazia(PilhaCircular *pilha) {
+int estaVazia(Pilha *pilha) {
     return (pilha->topo == -1);
 }
 
-// Empilhar na pilha circular
-void empilhar(PilhaCircular *pilha, int valor) {
+// Verifica se a pilha está cheia
+int estaCheia(Pilha *pilha) {
+    return (pilha->topo == TAMANHO - 1);
+}
+
+// Empilhar (push)
+void empilhar(Pilha *pilha, int valor) {
     if (estaCheia(pilha)) {
         printf("Pilha cheia!\n");
     } else {
-        pilha->topo = (pilha->topo + 1) % TAMANHO;
+        pilha->topo++;
         pilha->itens[pilha->topo] = valor;
         printf("Empilhado: %d\n", valor);
     }
 }
 
-// Desempilhar na pilha circular
-int desempilhar(PilhaCircular *pilha) {
+// Desempilhar (pop)
+int desempilhar(Pilha *pilha) {
     if (estaVazia(pilha)) {
         printf("Pilha vazia!\n");
         return -1;
     } else {
         int valor = pilha->itens[pilha->topo];
-        // Para indicar posição vazia
-        pilha->itens[pilha->topo] = -1;
-        pilha->topo = (pilha->topo - 1 + TAMANHO) % TAMANHO;
+        pilha->topo--;
         return valor;
     }
 }
 
-// Consultar o topo da pilha
-int consultarTopo(PilhaCircular *pilha) {
+// Consultar o topo (peek)
+int consultarTopo(Pilha *pilha) {
     if (estaVazia(pilha)) {
         printf("Pilha vazia!\n");
         return -1;
@@ -57,20 +55,16 @@ int consultarTopo(PilhaCircular *pilha) {
 }
 
 int main() {
-    PilhaCircular pilha;
+    Pilha pilha;
     inicializarPilha(&pilha);
 
     empilhar(&pilha, 10);
     empilhar(&pilha, 20);
     empilhar(&pilha, 30);
-    empilhar(&pilha, 40);
 
     printf("Topo da pilha: %d\n", consultarTopo(&pilha));
-
     desempilhar(&pilha);
-    printf("Topo após desempilhar: %d\n", consultarTopo(&pilha));
+    printf("Topo apos desempilhar: %d\n", consultarTopo(&pilha));
 
     return 0;
 }
-
-
